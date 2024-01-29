8d297504ddcc8957bf8752038ae7d8e1690bb73e
yusup <yusupessu@gmail.com>
2024-01-29 20:43:37 +0400

    Homework from 10th. Added StGroup and Student files

--- MainMenu.py
+++ MainMenu.py
@@ -7,15 +7,24 @@ class MainMenu(QMenuBar):
     def __init__(self, parent):
         super().__init__(parent)
 
-        help_menu = self.addMenu('Справка')
+        teacher_menu = self.addMenu('Учитель')
+        self.teacher_add = teacher_menu.addAction('Добавить')
+        self.teacher_edit = teacher_menu.addAction('Редактировать')
+        self.teacher_delete = teacher_menu.addAction('Удалить')
 
+        student_menu = self.addMenu('Студент')
+        self.student_add = student_menu.addAction('Добавить')
+        self.student_edit = student_menu.addAction('Редактировать')
+        self.student_delete = student_menu.addAction('Удалить')
+
+        stgroup_menu = self.addMenu('Группа')
+        self.stgroup_add = stgroup_menu.addAction('Добавить')
+        self.stgroup_edit = stgroup_menu.addAction('Редактировать')
+        self.stgroup_delete = stgroup_menu.addAction('Удалить')
+
+        help_menu = self.addMenu('Справка')
         self.__about = help_menu.addAction('О программе...')
         self.about_qt = help_menu.addAction('О библиотеке Qt...')
-
-        help_menu1 = self.addMenu('Учитель')
-        self.teacher_add = help_menu1.addAction('Добавить')
-        self.teacher_edit = help_menu1.addAction('Редактировать')
-        self.teacher_delete = help_menu1.addAction('Удалить')
 
     @property
     def about(self):
--- MainWindow.py
+++ MainWindow.py
@@ -1,7 +1,7 @@
 from PyQt6.QtWidgets import QMainWindow, QMessageBox
 from PyQt6.QtCore import pyqtSlot
 from MainMenu import MainMenu
-import Teacher
+import Teacher, Student, StGroup
 
 class MainWindow(QMainWindow):
 
@@ -11,15 +11,23 @@ class MainWindow(QMainWindow):
         main_menu = MainMenu(parent=self)
         self.setMenuBar(main_menu)
         
-        v = Teacher.View(parent=self)
+        v = StGroup.View(parent=self)
         self.setCentralWidget(v)
 
         main_menu.about_qt.triggered.connect(self.about_qt)
         main_menu.about.triggered.connect(self.about)
 
-        main_menu.teacher_add.triggered.connect(v.add)
-        main_menu.teacher_delete.triggered.connect(v.delete)
-        main_menu.teacher_edit.triggered.connect(v.update)
+        # main_menu.teacher_add.triggered.connect(v.add)
+        # main_menu.teacher_delete.triggered.connect(v.delete)
+        # main_menu.teacher_edit.triggered.connect(v.update)
+
+        # main_menu.student_add.triggered.connect(v.add)
+        # main_menu.student_delete.triggered.connect(v.delete)
+        # main_menu.student_edit.triggered.connect(v.update)
+
+        main_menu.stgroup_add.triggered.connect(v.add)
+        main_menu.stgroup_delete.triggered.connect(v.delete)
+        main_menu.stgroup_edit.triggered.connect(v.update)
 
     @pyqtSlot()
     def about(self):
