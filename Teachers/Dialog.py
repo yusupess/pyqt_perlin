9852b527ffb14ad8317d8c2e4d4d87bfae763184
yusup <yusupessu@gmail.com>
2024-03-19 08:26:47 +0400

    19th

--- Teachers/Dialog.py
+++ Teachers/Dialog.py
@@ -1,13 +1,23 @@
 from PyQt6.QtWidgets import QDialog
+from PyQt6.QtWidgets import QFrame
 from PyQt6.QtWidgets import QLabel, QLineEdit, QTextEdit, QPushButton
 from PyQt6.QtWidgets import QVBoxLayout, QHBoxLayout
 from PyQt6.QtCore import pyqtSlot
+
+from .Ui_TeacherFrame import Ui_TeacherFrame
 
 
 SELECT_ONE = """select f_fio, f_phone, f_email, f_comment
                 from teacher
                 where id = %s ;
             """
+
+class _Frame(QFrame):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.ui = Ui_TeacherFrame()
+        self.ui.setupUi(self)
 
 
 class Dialog(QDialog):
@@ -17,17 +27,7 @@ class Dialog(QDialog):
 
         self.setWindowTitle('Учитель')
 
-        fio_lbl = QLabel('Фамилия И. О.', parent=self)
-        self.__fio_edt = QLineEdit(parent=self)
-
-        phone_lbl = QLabel('Телефон', parent=self)
-        self.__phone_edt = QLineEdit(parent=self)
-
-        email_lbl = QLabel('e-mail', parent=self)
-        self.__email_edt = QLineEdit(parent=self)
-
-        comment_lbl = QLabel('Примечание', parent=self)
-        self.__comment_edt = QTextEdit(parent=self)
+        self.__frame = _Frame(parent=self)
 
         ok_btn = QPushButton('OK', parent=self)
         cancel_btn = QPushButton('Отмена', parent=self)
@@ -35,35 +35,8 @@ class Dialog(QDialog):
         lay = QVBoxLayout(self)
         lay.setSpacing(15)
 
-        lay_fam = QVBoxLayout()
-        # наводим красоту. устанавливаем расстояние м\у элементами
-        lay_fam.setSpacing(0)
-        lay_fam.addWidget(fio_lbl)
-        lay_fam.addWidget(self.__fio_edt)
-        lay.addLayout(lay_fam)
-
-        lay3 = QHBoxLayout()
-        
-        lay_phn = QVBoxLayout()
-        lay_phn.setSpacing(0)
-        lay_phn.addWidget(phone_lbl)
-        lay_phn.addWidget(self.__phone_edt)
-        lay3.addLayout(lay_phn)
-        
-        lay_em = QVBoxLayout()
-        lay_em.setSpacing(0)
-        lay_em.addWidget(email_lbl)
-        lay_em.addWidget(self.__email_edt)
-        lay3.addLayout(lay_em)
-        
-        lay.addLayout(lay3)
-
-        lay_com = QVBoxLayout()
-        lay_com.setSpacing(0)
-        lay_com.addWidget(comment_lbl)
-        lay_com.addWidget(self.__comment_edt)
-        lay.addLayout(lay_com)
-        
+        lay.addWidget(self.__frame)
+     
         # горизонтальный слой для кнопок "ОК" и "Отмена"
         lay2 = QHBoxLayout()
         # доавляем пустое пространство чтобы кнопки ок и Отмена были в правом углу
@@ -87,7 +60,7 @@ class Dialog(QDialog):
 
     @property
     def fio(self):
-        result = self.__fio_edt.text().strip()
+        result = self.__frame.ui.fio_edt.text().strip()
         # if not result:
         #     return None
         # return result
@@ -96,11 +69,11 @@ class Dialog(QDialog):
     
     @fio.setter
     def fio(self, value):
-        self.__fio_edt.setText(value)
+        self.__frame.ui.fio_edt.setText(value)
     
     @property
     def phone(self):
-        result = self.__phone_edt.text().strip()
+        result = self.__frame.ui.phone_edt.text().strip()
         # if not result:
         #     return None
         # return result
@@ -109,12 +82,12 @@ class Dialog(QDialog):
         
     @phone.setter
     def phone(self, value):
-        self.__phone_edt.setText(value)
+        self.__frame.ui.phone_edt.setText(value)
     
     @property
     def comment(self):
         # toPlainText() выполняет ту же функцию что и text()
-        result = self.__comment_edt.toPlainText().strip()
+        result = self.__frame.ui.comment_edt.toPlainText().strip()
         # if not result:
         #     return None
         # return result
@@ -123,14 +96,26 @@ class Dialog(QDialog):
     
     @comment.setter
     def comment(self, value):
-        self.__comment_edt.setPlainText(value)
+        self.__frame.ui.comment_edt.setPlainText(value)
 
     @property
     def email(self):
-        result = self.__email_edt.text().strip()
+        result = self.__frame.ui.email_edt.text().strip()
         if result:
             return result
         
     @email.setter
     def email(self, value):
-        self.__email_edt.setText(value)
+        self.__frame.ui.email_edt.setText(value)
+
+    @property
+    def login(self):
+        result = self.__frame.ui.login_edt.text().strip()
+        if result == '':
+            return None
+        else:
+            return result
+        
+    @login.setter
+    def login(self, value):
+        self.__frame.ui.login_edt.setText(value)
