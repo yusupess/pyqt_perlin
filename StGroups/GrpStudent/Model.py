14a9936a9de72eabe8792d9d38cc3eb4d0d94ec8
yusup <yusupessu@gmail.com>
2024-04-03 15:50:09 +0400

    43

--- StGroups/GrpStudent/Model.py
+++ StGroups/GrpStudent/Model.py
@@ -1,7 +1,32 @@
-from PyQt6.QtCore import QAbstractTableModel
+from PyQt6.QtCore import Qt, QAbstractTableModel, QModelIndex
+import psycopg2
+import settings as st
+
+
+# SELECT_ALL = """select * from student_"""
+
 
 
 class Model(QAbstractTableModel):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.refill()
+
+    def rowCount(self, parent_index) -> int:
+        if parent_index.isValid():
+            return 0
+        return 10
+    
+    def columnCount(self, parent_index) -> int:
+        return 0 if parent_index.isValid() else 1
+    
+    def data(self, index, role):
+        if role == Qt.ItemDataRole.DisplayRole:
+            r = index.row()
+            c = index.column()
+            return f'{r=}, {c=}'
+        else:
+            return None
+        
+    def refill(sef):
--- StGroups/GrpStudent/View.py
+++ StGroups/GrpStudent/View.py
@@ -1,8 +1,12 @@
 from PyQt6.QtWidgets import QTableView
 
+from .Model import Model
 
 class View(QTableView):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+
+        model = Model(parent=self)
+        self.setModel(model)
         
